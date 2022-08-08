c8a3eeacac79edbadcfa873034f647600f06e665
ikhan1 <ishratkhan@rocketmortgage.com>
2022-08-08 16:33:24 -0400

    updated about me page

---
+++ src/components/Navigation.js
@@ -0,0 +1,24 @@
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import "../styles/Navigation.css";
+
+const Navigation = () => {
+  return (
+    <Breadcrumbs aria-label="breadcrumb">
+      <Link underline="hover" color="inherit" href="/">
+        About Me
+      </Link>
+      <Link underline="hover" color="inherit" href="/portfolio">
+        Portfolio
+      </Link>
+      <Link underline="hover" color="inherit" href="/resume">
+        Resume
+      </Link>
+      <Link underline="hover" color="inherit" href="/contact">
+        Contact
+      </Link>
+    </Breadcrumbs>
+  );
+};
+
+export default Navigation;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,29 @@
+import Navigation from "../components/Navigation";
+import "../styles/AboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <>
+      <Navigation />
+      <div className="aboutMe-container">
+        <div className="left-container">
+          <img src="Image1.png" />
+        </div>
+        <div className="right-container">
+          <p>Hello!!</p>
+          <h1>This is portfolio</h1>
+          <p>
+            I am a student at the University of Michigan - Ann Arbor, where I am
+            studying computer engineering.
+          </p>
+          <p>
+            Some of my hobbies include baking, dancing, and playing computer
+            games.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AboutMe;
